7fb418559ba6dad914f54bf454e86358075a47c0
LS2002 <lukesunca@gmail.com>
2017-04-13 18:35:31 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,10 +1,5 @@
 print 'hello'
 print 'hello2'
 print 'hello3'
-#<<<<<<< dev
-#print 'mod from dev branch'
-#=======
-#print 'modified from master'
-#>>>>>>> master
 print 'merged dev and master'
 print "new change from dev'
